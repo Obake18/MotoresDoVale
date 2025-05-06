f3606039d58e05d45672c134b526d618f5ae7426
Obake18 <coimbrajuniorm@gmail.com>
2025-05-06 13:36:15 -0300

    Função do Whatsapp

---
+++ motoresdovaleonline/src/app/componentes/contato.js
@@ -0,0 +1,16 @@
+import { FaWhatsapp } from "react-icons/fa";
+
+export default function Contato() {
+  const numeroWhatsApp = "5574981166046"; // Coloque aqui o número com DDI (55 para Brasil) e DDD
+
+  const linkWhatsApp = `https://wa.me/${numeroWhatsApp}`;
+
+  return (
+    <div className="contato">
+      <a href={linkWhatsApp} target="_blank" rel="noopener noreferrer" className="botao-whatsapp">
+        <FaWhatsapp className="icone-contato" />
+        <span>Fale conosco pelo WhatsApp</span>
+      </a>
+    </div>
+  );
+}
--- motoresdovaleonline/src/app/page.js
+++ motoresdovaleonline/src/app/page.js
@@ -1,16 +1,21 @@
 import TextList from "./componentes/textos";
 export default function Home() {
   return (
-   <div className="titulo">
-    <div>
-      <h1>
-        Motores do Vale
-      </h1>
+    <>
+      <div className="titulo">
+        <div>
+            <h1>Motores do Vale</h1>
+        </div>
+        <div>
+          
+        </div>
+
+      </div>
+
       <div>
         
+        <TextList />
       </div>
-    </div>
-    <TextList/>
-   </div>
+    </>
   );
 }
