2fb933840a82d295eba92f65a8b0feff9162a495
Obake18 <coimbrajuniorm@gmail.com>
2025-05-06 19:01:16 -0300

    Botão do instagram, pq não?

--- motoresdovaleonline/src/app/componentes/contato.js
+++ motoresdovaleonline/src/app/componentes/contato.js
@@ -1,15 +1,20 @@
-import { FaWhatsapp } from "react-icons/fa";
+import { FaWhatsapp, FaInstagram } from "react-icons/fa";
 
 export default function Contato() {
-  const numeroWhatsApp = "5574981166016"; // Coloque aqui o número com DDI (55 para Brasil) e DDD
-
+  const numeroWhatsApp = "5574981166016";
   const linkWhatsApp = `https://wa.me/${numeroWhatsApp}`;
+  const linkInstagram = "https://www.instagram.com/motoresdovaleof/"; // Substitua pelo seu @
 
   return (
     <div className="contato">
       <a href={linkWhatsApp} target="_blank" rel="noopener noreferrer" className="botao-whatsapp">
         <FaWhatsapp className="icone-contato" />
         <span>Fale conosco pelo WhatsApp</span>
+      </a>
+
+      <a href={linkInstagram} target="_blank" rel="noopener noreferrer" className="botao-instagram">
+        <FaInstagram className="icone-contato" />
+        <span>Chame a empresa no Instagram</span>
       </a>
     </div>
   );
