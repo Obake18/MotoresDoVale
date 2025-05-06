23281b9969382a7ba4f830dd56b72735da57d6b5
Obake18 <coimbrajuniorm@gmail.com>
2025-05-06 19:15:21 -0300

    Protótipo de botões

--- motoresdovaleonline/src/app/componentes/contato.js
+++ motoresdovaleonline/src/app/componentes/contato.js
@@ -1,4 +1,5 @@
 import { FaWhatsapp, FaInstagram } from "react-icons/fa";
+import styles from "./contato.module.css";
 
 export default function Contato() {
   const numeroWhatsApp = "5574981166016";
@@ -6,14 +7,14 @@ export default function Contato() {
   const linkInstagram = "https://www.instagram.com/motoresdovaleof/";
 
   return (
-    <div className="contato">
-      <a href={linkWhatsApp} target="_blank" rel="noopener noreferrer" className="botao-whatsapp">
-        <FaWhatsapp className="icone-contato" />
+    <div className={styles.contato}>
+      <a href={linkWhatsApp} target="_blank" rel="noopener noreferrer" className={styles["botao-whatsapp"]}>
+        <FaWhatsapp className={styles["icone-contato"]} />
         <span>Fale conosco pelo WhatsApp</span>
       </a>
 
-      <a href={linkInstagram} target="_blank" rel="noopener noreferrer" className="botao-instagram">
-        <FaInstagram className="icone-contato" />
+      <a href={linkInstagram} target="_blank" rel="noopener noreferrer" className={styles["botao-instagram"]}>
+        <FaInstagram className={styles["icone-contato"]} />
         <span>Siga nosso Instagram</span>
       </a>
     </div>
